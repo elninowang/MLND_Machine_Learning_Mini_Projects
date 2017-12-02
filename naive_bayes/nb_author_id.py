f56c19d1c3e4441dd15fac3b0c551c54f6127f93
ElNino Wang <elninowang@gmail.com>
2017-12-02 22:38:47 +0800

    complete naive_bayes mini project

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -15,17 +15,32 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
-
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
+print(features_train.shape)
+print(features_test.shape)
+print(len(labels_train))
+print(len(labels_test))
+
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+t0 = time()
+gnb = GaussianNB()
+gnb_model = gnb.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t1 = time()
+lables_pred = gnb_model.predict(features_test)
+print "predicting time:", round(time()-t1, 3), "s"
+
+accuracy = accuracy_score(labels_test, lables_pred)
+print("accuracy: {}".format(accuracy))
+
 
 
 #########################################################
