f137c7c19b593a5ff760cac60d6dac2b9398c114
ElNino Wang <elninowang@gmail.com>
2017-12-03 22:22:56 +0800

    complete choose_your_own mini project

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -24,21 +24,54 @@ plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
 plt.legend()
 plt.xlabel("bumpiness")
 plt.ylabel("grade")
+plt.savefig('temp.png')
 plt.show()
 ################################################################################
-
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+print "features_train: {}".format(len(features_train))
+print "features_test: {}".format(len(features_test))
+print "labels_train: {}".format(len(labels_train))
+print "labels_test: {}".format(len(labels_test))
 
-
-
-
-
-
+from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier, GradientBoostingClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 
 try:
-    prettyPicture(clf, features_test, labels_test)
+    clf = AdaBoostClassifier(n_estimators=100)
+    clf = clf.fit(features_train, labels_train)
+    accuracy = accuracy_score(labels_test, clf.predict(features_test))
+    print "AdaBoostClassifier accuracy: {}".format(accuracy)
+    prettyPicture(clf, features_test, labels_test, "adaboost_test.png")
+except NameError:
+    pass
+
+try:
+    clf = RandomForestClassifier()
+    clf = clf.fit(features_train, labels_train)
+    accuracy = accuracy_score(labels_test, clf.predict(features_test))
+    print "RandomForestClassifier accuracy: {}".format(accuracy)
+    prettyPicture(clf, features_test, labels_test, "randomforest_test.png")
+except NameError:
+    pass
+
+try:
+    clf = GradientBoostingClassifier()
+    clf = clf.fit(features_train, labels_train)
+    accuracy = accuracy_score(labels_test, clf.predict(features_test))
+    print "GradientBoostingClassifier accuracy: {}".format(accuracy)
+    prettyPicture(clf, features_test, labels_test, "gradientboosting_test.png")
+except NameError:
+    pass
+
+try:
+    clf = KNeighborsClassifier(n_neighbors=10)
+    clf = clf.fit(features_train, labels_train)
+    prettyPicture(clf, features_test, labels_test, "knn_test.png")
+    print "KNeighborsClassifier accuracy: {}".format(accuracy)
+    prettyPicture(clf, features_test, labels_test, "gradientboosting_test.png")
 except NameError:
     pass
